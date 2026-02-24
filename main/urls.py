1d514487db138b1d2f7f814b1dd6934d8b411731
mub1nakhan <arkadashler82@gmail.com>
2026-02-24 09:00:55 +0500

    taomlar sahifasi ishga tushirildi

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("contact", views.contact_veiw, name="contacts"),
-    path("meal/<int:pk>",views.meal_detail, name="meal_detail")
+    path("meal/<int:pk>",views.meal_detail, name="meal_detail"),
+    path("meals/", views.meals_view, name="meals"),
+    path("add-meal/", views.add_meal, name="add_meal"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,42 +1,86 @@
-from django.shortcuts import render,redirect
-
-from .models import Comment,Meal
-
+from django.shortcuts import render, redirect
+from .models import Meal, Comment,Ingredient
 
 
 
 def index_view(request):
-    meals = Meal.objects.filter(status = "active")
-    context = {"meals":meals}
-    
+
+    meals = Meal.objects.filter(status="active")
+
+    context = {
+        "meals": meals
+    }
+
     return render(request, "main.html", context)
 
 
-def meal_detail(request,pk):
+def meal_detail(request, pk):
+
     meal = Meal.objects.get(id=pk)
-    
-    
+
     comments = Comment.objects.filter(meal=meal).order_by("-created_at")
-    context = {"meal":meal,
-               "comments":comments}
-    
-    
-    
-    if request.method=="POST":
-        author=request.POST.get("author")
-        body=request.POST.get("body")
-        
+
+    if request.method == "POST":
+
+        author = request.POST.get("author")
+
+        body = request.POST.get("body")
+
         Comment.objects.create(
+            meal=meal,
             author=author,
-            body=body,
-            meal=meal
+            body=body
         )
-        return redirect("meal_detail",meal.id)
-    return render(request,"meal_detail.html",context)
-    
 
+        return redirect("meal_detail", meal.id)
 
+    context = {
+        "meal": meal,
+        "comments": comments
+    }
+
+    return render(request, "meal_detail.html", context)
 
 
 def contact_veiw(request):
+
     return render(request, "contact.html")
+
+def meals_view(request):
+
+    meals = Meal.objects.all()
+
+    context = {
+        "meals": meals
+    }
+
+    return render(request, "meals.html", context)
+
+
+def add_meal(request):
+
+    ingredients = Ingredient.objects.all()
+
+    if request.method == "POST":
+
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        image = request.FILES.get("image")
+
+        meal = Meal.objects.create(
+            name=name,
+            description=description,
+            image=image
+        )
+
+        selected_ingredients = request.POST.getlist("ingredients")
+
+        meal.ingredients.set(selected_ingredients)
+
+        return redirect("meals")
+
+    context = {
+        "ingredients": ingredients
+    }
+
+    return render(request, "add_meal.html", context)
